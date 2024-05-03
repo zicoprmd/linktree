7d53b33bd9c93de2f6292d6de6893d41ed81f8a1
zicoprmd <zicoprmd@gmail.com>
2024-05-03 08:56:20 +0700

    edit colour background

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-window.onload=function(){
+window.onload = function () {
   const toggle = document.getElementById('switch');
   const body = document.body;
 
-  toggle.addEventListener('input', e => {
-    console.log("here")
-      const isChecked = e.target.checked;
+  toggle.addEventListener('input', (e) => {
+    console.log('dar mode');
+    const isChecked = e.target.checked;
+    const hover = document.getElementById('a:hover');
 
-      if (isChecked) { // light mode
-          body.classList.remove('dark-mode-body');
-          body.classList.add('light-mode-body');
-          [ ...document.getElementsByClassName("dark-mode-a") ].forEach( x => x.className = 'light-mode-a')
-          document.getElementById("divider-pic").src="divider_light.svg";
-          document.getElementById("slider-circle").className="slider round slider-mode-dark";
-      } else { // dark mode
-          body.classList.remove('light-mode-body');
-          body.classList.add('dark-mode-body');
-          [ ...document.getElementsByClassName("light-mode-a") ].forEach( x => x.className = 'dark-mode-a');
-          document.getElementById("divider-pic").src="divider_dark.svg";
-          document.getElementById("slider-circle").className="slider round slider-mode-light";
-      }
+    if (isChecked) {
+      // light mode
+      body.classList.remove('dark-mode-body');
+      body.classList.add('light-mode-body');
+      [...document.getElementsByClassName('dark-mode-a')].forEach(
+        (x) => (x.className = 'light-mode-a')
+      );
+      document.getElementById('divider-pic').src = 'divider_light.svg';
+      document.getElementById('slider-circle').className =
+        'slider round slider-mode-dark';
+    } else {
+      // dark mode
+      body.classList.remove('light-mode-body');
+      body.classList.add('dark-mode-body');
+      [...document.getElementsByClassName('light-mode-a')].forEach(
+        (x) => (x.className = 'dark-mode-a')
+      );
+      document.getElementById('divider-pic').src = 'divider_dark.svg';
+      document.getElementById('slider-circle').className =
+        'slider round slider-mode-light';
+    }
   });
-}
+};
